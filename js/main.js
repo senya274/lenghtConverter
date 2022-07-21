4a88b3a5a02ce60263cf640eec056123e46a9653
senya274 <senya2470@gmail.com>
2022-07-21 11:35:12 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+let input = document.getElementById('input');
+let result = document.getElementById('result');
+let inputType = document.getElementById('inputType');
+let resultType = document.getElementById('resultType');
+let inputTypeValue, resultTypeValue
+
+input.addEventListener("keyup", myResult)
+inputType.addEventListener("change", myResult)
+resultType.addEventListener("change", myResult)
+
+inputTypeValue = inputType.value;
+resultTypeValue = resultType.value;
+
+function myResult() {
+
+    inputTypeValue = inputType.value;
+    resultTypeValue = resultType.value;
+
+    if (inputTypeValue === "meter" && resultTypeValue === "kilometer") {
+        result.value = Number(input.value) * 0.001;
+    }
+    else if
+        (inputTypeValue === "meter" && resultTypeValue === "centimeter") {
+        result.value = Number(input.value) * 100;
+    }
+    else if (inputTypeValue === "meter" && resultTypeValue === "meter") {
+        result.value = input.value
+    }
+
+    if (inputTypeValue === "kilometer" && resultTypeValue === "meter") {
+        result.value = Number(input.value) * 1000;
+    } else if (inputTypeValue === "kilometer" && resultTypeValue === "centimeter") {
+        result.value = Number(input.value) * 100000;
+    } else if (inputTypeValue === "kilometer" && resultTypeValue === "kilometer") {
+        result.value = input.value
+    }
+
+    if (inputTypeValue === "Centimeter" && resultTypeValue === "kilometer") {
+        result.value = Number(input.value) * 1000;
+    } else if (inputTypeValue === "Centimeter" && resultTypeValue === "meter") {
+        result.value = Number(input.value) * 100000;
+    } else if (inputTypeValue === "Centimeter" && resultTypeValue === "Centimeter") {
+        result.value = input.value
+    }
+}
+
+
